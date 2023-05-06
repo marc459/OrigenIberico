927bcb067d1976a4ed85853a9465b70bde057fc9
Marcos Santos Serrano <santosserranomarcos@gmail.com>
2023-05-06 19:51:09 +0200

    gihub pages

---
+++ frontend/src/pages/Auth.js
@@ -0,0 +1,41 @@
+import './Auth.css';
+import React, { useState } from "react";
+import { Login } from "../components/login";
+import { Register } from "../components/register";
+
+function Navbar(props) {
+  const { onLoginClick, onRegisterClick } = props;
+
+  return (
+    <div className="navbar">
+      <img src="./imgs/logo-svg.svg" alt="Logo" className="logo" />
+      <div className="menu">
+        <button onClick={onLoginClick}>Login</button>
+        <button onClick={onRegisterClick}>Register</button>
+      </div>
+    </div>
+  );
+}
+
+export function Auth() {
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+
+  const handleLoginClick = () => {
+    setShowLogin(true);
+    setShowRegister(false);
+  };
+
+  const handleRegisterClick = () => {
+    setShowRegister(true);
+    setShowLogin(false);
+  };
+
+  return (
+    <div className="App">
+      <Navbar onLoginClick={handleLoginClick} onRegisterClick={handleRegisterClick} />
+      {showLogin && <Login />}
+      {showRegister && <Register />}
+    </div>
+  );
+}
---
+++ frontend/src/routes/Routes.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Routes, Route, BrowserRouter} from 'react-router-dom';
+import { Home } from "../pages/Home";
+import { Auth } from "../pages/Auth";
+import { About } from "../pages/About";
+
+export default function App() {
+  return (
+    <div className="App">
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Auth />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
