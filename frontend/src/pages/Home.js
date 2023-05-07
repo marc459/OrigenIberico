c8640149666627239cd86c5e3af74201e8674e43
Marcos Santos Serrano <santosserranomarcos@gmail.com>
2023-05-07 05:54:13 +0200

    search cow

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
+import { useNavigate } from 'react-router-dom';
 
 export function Home() {
+  const navigate = useNavigate();
   return (
     <div className="home">
       <h1>Busca a tu vaca con su Código de identificación</h1><br></br>
       <input type='text'></input>
-      <span className='search'><button class="btn-search"></button></span><br></br>
+      <span className='search'><button class="btn-search" onClick={() => {navigate('/cow/cow1', {replace: true})}}></button></span><br></br>
       <Link to="/about">About</Link>
     </div>
   );
