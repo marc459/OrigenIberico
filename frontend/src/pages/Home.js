5020c752560645c1da9d101fe6c9597613a4fdeb
Marcos Santos Serrano <santosserranomarcos@gmail.com>
2023-05-07 02:38:05 +0200

    grid cows home

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,7 +5,17 @@ import './Home.css';
 export function Home() {
   return (
     <div className="home">
-      <h1>Home page</h1>
+      <div className="home-grid">
+          <div class="cow-card">
+            <img alt="" src="./imgs/cow1.png"/>
+            <h2>Barbara</h2>
+            <p>Jaén</p>
+            <p>Fecha de muerte: 30-04-2023</p>
+            <p>Alimentada a base de pienso ecológico</p>
+            <span class="left bottom">tel: 731 366 ***</span>
+            <span class="right bottom">adress: Czech Republic</span>
+          </div>
+      </div>
       <Link to="/about">About</Link>
     </div>
   );
